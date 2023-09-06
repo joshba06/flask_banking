93d008db9edf1f33d8a772430556ed4fed3d60f2
Niklas Bakula <niklasjbakula@gmail.com>
2023-09-06 14:37:28 +0200

    Updated config with os.environ.get

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ class Config:
     # Load secret vars from env file
     load_dotenv()
 
-    IS_PROD = os.getenv('IS_HEROKU', None)
+    IS_PROD = os.environ.get('IS_HEROKU', None)
 
     ## PRODUCTION config
     if IS_PROD == "True" or IS_PROD is True:
-        DATABASE_URL=os.getenv("DATABASE_URL_HEROKU")
-        SECRET_KEY=os.getenv("SECRET_KEY_HEROKU"),
+        DATABASE_URL=os.environ.get("DATABASE_URL_HEROKU")
+        SECRET_KEY=os.environ.get("SECRET_KEY_HEROKU"),
 
     ## DEVELOPMENT config
     else:
